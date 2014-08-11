715154d77b4df2de8582ef0aab03748d92f388d4
Amy Vaillancourt-Sals & Andrew M Westling <student@epicodus.com>
2014-08-11 15:18:54 -0700

    add logic to display train stations for passenger

--- darjeeling.rb
+++ darjeeling.rb
@@ -1,4 +1,15 @@
+require './lib/train'
+require './lib/station'
+require 'pg'
+require 'pry'
+
+DB = PG.connect({:dbname => 'darjeeling'})
+
+@current_train = nil
+
 def who_are_you
+
+  system("clear")
 
   puts "Welcome to the Darjeeling Limited. Are you a passenger or a train conductor?"
   puts "Press 'p' for passenger, or 'c' for conductor."
@@ -9,39 +20,78 @@ def who_are_you
   elsif user_input == 'c'
     conductor_menu
   else
-    puts "Invalid input, please try again."
+    puts "\nInvalid input, please try again.\n"
+    sleep(1)
     who_are_you
   end
 end
 
 def passenger_menu
-  puts "Which list would you like to refer to?"
+  puts "\n\nWhich list would you like to refer to?"
   puts "Press 't' for trains, 's' for stations, or any other key to exit."
   user_input = gets.chomp
 
   if user_input == 't'
-    p_train_menu
+    train_menu(:passenger)
   elsif user_input == 's'
-    p_station_menu
+    station_menu(:passenger)
   else
-    puts "See you next time!"
+    puts "\nSee you next time!"
     exit
   end
 end
 
 def conductor_menu
-  puts "Which list would you like to access?"
+  puts "\n\nWhich list would you like to access?"
   puts "Press 't' for trains, 's' for stations, or any other key to exit."
   user_input = gets.chomp
 
   if user_input == 't'
-    c_train_menu
+    train_menu(:conductor)
   elsif user_input == 's'
-    c_station_menu
+    station_menu(:conductor)
   else
-    puts "See you next time!"
+    puts "\nSee you next time!"
     exit
   end
+end
+
+
+def train_menu who
+
+  puts "All Trains:"
+  puts Train.show_list
+
+  case who
+  when :passenger
+    puts "Pick a train line to see which stations it stops at"
+    puts "Enter the number for the train to look up:"
+    user_choice = gets.chomp
+    @current_train = Train.all.fetch((user_choice.to_i)-1) { |i| puts "#{i+1} is not a valid train. Please try again.\n\n"
+    train_menu(:passenger)}
+
+    @current_train.stations.each do |station|
+      puts "#{station.name}"
+    end
+
+  when :conductor
+    puts "you're a conductor"
+  end
+
+end
+
+def station_menu who
+
+  puts "All Stations:"
+  puts Station.show_list
+
+  case who
+  when :passenger
+    puts "you're just a passenger"
+  when :conductor
+    puts "you're a conductor"
+  end
+
 end
 
 who_are_you
